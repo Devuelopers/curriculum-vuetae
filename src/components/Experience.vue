c6ce5539cab733c173bd8ff5f1caab3e39f8fb1c
TheLe0 <leob.tosin@hotmail.com>
2021-11-02 16:16:51 -0300

    Some adjustments

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,17 +1,28 @@
 <template>
     <div class="entry">
-        <h2>EXPERIENCE</h2>
-        <div class="content">
-            <h3> From - To</h3>
-            <p>Role<br />
-            <em>Company</em>
-            <b>Stack:</b> Vue, Typescript and Azure
-            </p>
-            <ul class="info">
-                <li>An activity I did</li>
-                <li>Some another activity</li>
-            </ul>
-        </div>
+      <h2>EXPERIENCE</h2>
+      <div class="content">
+        <h3> From - currently</h3>
+        <p>Role<br />
+        <em>Company</em>
+        <b>Stack:</b> Vue, Typescript and Azure
+        </p>
+        <ul class="info">
+          <li>An activity I did</li>
+          <li>Some another activity</li>
+        </ul>
+      </div>
+      <div class="content">
+        <h3> From - To</h3>
+        <p>Role<br />
+        <em>Company</em>
+        <b>Stack:</b> Vue, Typescript and Azure
+        </p>
+        <ul class="info">
+          <li>An activity I did</li>
+          <li>Some another activity</li>
+        </ul>
+      </div>
     </div>
 </template>
 
--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -1,7 +1,9 @@
 <template>
     <h2>PRESENTATION</h2>
     <div class="paragraph">
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</p>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</p>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</p>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</p>
     </div>
 </template>
 
