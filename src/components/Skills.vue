a21baa38f854bb1a0ba48b722fc44a9e1a1af298
TheLe0 <leob.tosin@hotmail.com>
2021-11-02 16:20:04 -0300

    Fix some typos

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="entry">
         <ul class="works">
-          <li><a><img src="../assets/azure.jpg" alt="Azure"></a></li>
-          <li><a><img src="../assets/azure_functions.png" alt="Azure Functions"></a></li>
-          <li><a><img src="../assets/typescript.png" alt="Typescript"></a></li>
-          <li><a><img src="../assets/vue.png" alt="Vue"></a></li>
+            <li><a><img src="../assets/azure.jpg" alt="Azure"></a></li>
+            <li><a><img src="../assets/azure_functions.png" alt="Azure Functions"></a></li>
+            <li><a><img src="../assets/typescript.png" alt="Typescript"></a></li>
+            <li><a><img src="../assets/vue.png" alt="Vue"></a></li>
         </ul>
     </div>
 </template>
